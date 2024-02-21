55100bab4172ec9ae8f2ccd623ae89363c53a8a0
Joseph <joseph.loustau@epfl.ch>
2024-02-21 09:20:43 +0100

    Add examples for the new features

---
+++ scripts/examples/3i_Custom_profiles.py
@@ -0,0 +1,35 @@
+from reho.model.reho import *
+
+
+if __name__ == '__main__':
+
+    # Set building parameters
+    # you can as well define your district from a csv file instead of reading the database
+    reader = QBuildingsReader()
+    qbuildings_data = reader.read_csv(buildings_filename='buildings_example.csv', nb_buildings=2)
+
+    # Select weather data
+    cluster = {'Location': 'Geneva', 'Attributes': ['I', 'T', 'W'], 'Periods': 10, 'PeriodDuration': 24}
+
+    # Set scenario
+    scenario = dict()
+    scenario['Objective'] = 'TOTEX'
+    scenario['name'] = 'totex'
+    scenario['exclude_units'] = ['Battery', 'NG_Cogeneration']
+    scenario['enforce_units'] = []
+
+    # Initialize available units and grids
+    grids = infrastructure.initialize_grids()
+    units = infrastructure.initialize_units(scenario, grids=grids)
+
+    # Set method options
+    # In that, give an electricity consumption profile
+    path_to_custom_elec_profile = '../../reho/data/electricity/annual_profiles.csv'
+    method = {'building-scale': True, 'use_custom_profiles': {'electricity': path_to_custom_elec_profile}}
+
+    # Run optimization
+    reho = reho(qbuildings_data=qbuildings_data, units=units, grids=grids, cluster=cluster, scenario=scenario, method=method, solver="gurobi")
+    reho.single_optimization()
+
+    # Save results
+    reho.save_results(format=['xlsx', 'pickle'], filename='3i')

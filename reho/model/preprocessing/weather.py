d97e551f180e22068dab78ca1d10c9f0b48e989e
Dorsan Lepour <dorsan.lepour@epfl.ch>
2025-01-13 16:45:34 +0100

    parsing issue with pvlib

--- reho/model/preprocessing/weather.py
+++ reho/model/preprocessing/weather.py
@@ -20,7 +20,7 @@ def get_weather_data(qbuildings_data, export_filename=None):
     """
     lat, long = Transformer.from_crs("EPSG:2056", "EPSG:4326").transform(qbuildings_data['buildings_data']['Building1']['x'], qbuildings_data['buildings_data']['Building1']['y'])
 
-    pvgis_data = pvlib.iotools.get_pvgis_tmy(lat, long, outputformat='csv', startyear=2005, endyear=2016)
+    pvgis_data = pvlib.iotools.get_pvgis_tmy(lat, long, startyear=2005, endyear=2016)
     coordinates = pvgis_data[2]
     weather_data = pvgis_data[0]
 

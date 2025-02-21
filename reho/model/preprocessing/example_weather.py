2b590030e7db0ed74e219ff6597592c3fabcbe66
milena553 <milenica0211@gmail.com>
2025-02-21 15:34:50 +0100

    example_weather added

---
+++ reho/model/preprocessing/example_weather.py
@@ -0,0 +1,21 @@
+from reho.model.reho import *
+from reho.model.preprocessing.weather import *
+
+if __name__ == '__main__':
+
+    reader = QBuildingsReader()
+    reader.establish_connection('Geneva')  # connect to QBuildings database
+    qbuildings_data = reader.read_db(district_id=5, egid=['2034144/2034143/2749579/2034146/2034145'])  # read data
+
+    Attributes = ['Text', 'Irr']
+    nb_clusters = [2, 4, 6, 8, 10, 12]
+
+    df_annual = get_weather_data(qbuildings_data).reset_index(drop=True)
+    df_annual = df_annual[Attributes]
+
+    cl = Clustering(data=df_annual, nb_clusters=nb_clusters, period_duration=24, options={"year-to-day": True, "extreme": []})
+    cl.run_clustering()
+
+    plot_cluster_KPI_separate(cl.kpis_clu, save_fig=False)
+    plot_LDC(cl, save_fig=False)
+

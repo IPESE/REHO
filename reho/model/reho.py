54dce105938f75e06bacf11b32f91a8f549d33ef
gouveia <catarina.braz@epfl.ch>
2025-12-15 15:40:24 +0100

    pool=30

--- reho/model/reho.py
+++ reho/model/reho.py
@@ -93,8 +93,7 @@ class REHO(MasterProblem):
     def execute_dantzig_wolfe_decomposition(self, scenario, Scn_ID, Pareto_ID=0, epsilon_init=None):
 
         # Initiation
-        self.pool = mp.Pool(mp.cpu_count())
-        print(self.pool)
+        self.pool = 28 #mp.Pool(mp.cpu_count())
         self.iter = 0  # new scenario has to start at iter = 0
         scenario, SP_scenario, SP_scenario_init = self.select_SP_obj_decomposition(scenario)
 
